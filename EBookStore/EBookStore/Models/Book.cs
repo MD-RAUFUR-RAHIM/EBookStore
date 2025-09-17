b03cffeff4eb547cfa61c61485e53dfea78324d4
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-09-18 02:35:59 +0600

    Configured Project and Created Entities with database update

---
+++ EBookStore/EBookStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EBookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string? BookName { get; set; }
+        [Required]
+        public double  Price { get; set; }
+        public string? BookImage { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        public List<OrderDetails> OrderDetails { get; set; }
+        public List<CartDetail> CartDetail { get; set; }
+
+    }
+}
