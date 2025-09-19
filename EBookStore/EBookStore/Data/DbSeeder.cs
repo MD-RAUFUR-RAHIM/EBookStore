e5883f99eacbc9dd4a01b7fec329710352c1c685
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-09-19 19:42:01 +0600

    feat(auth): add role enum and seed default roles
    Created new Role enum for user management and seeded initial roles (Admin, User) into the database for future use.

---
+++ EBookStore/EBookStore/Data/DbSeeder.cs
@@ -0,0 +1,32 @@
+﻿using EBookStore.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace EBookStore.Data
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDefaultData(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var userInDb = await userManager.FindByEmailAsync(admin.Email);
+
+            if (userInDb is null)
+            {
+                await userManager.CreateAsync(admin, "Admin@123");
+                await userManager.AddToRoleAsync(admin,Roles.Admin.ToString());
+            }
+        }
+    }
+}
